77f0cb54db0824ca700aba1ecb5b06a54c5c8f46
rafi983 <rafiirfan211@gmail.com>
2021-09-10 01:45:15 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ const changeBgColor = (color) => {
   const sec = document.querySelector(".sec");
   sec.style.backgroundColor = color;
 };
+
+function toggleMenu() {
+  const toggleMenu = document.querySelector(".toggleMenu");
+  const navigation = document.querySelector(".navigation");
+  toggleMenu.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
